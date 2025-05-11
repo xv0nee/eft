26b477c057365b8cf69c121278497c05ae1f9fd0
xv1 <149095757+xv0nee@users.noreply.github.com>
2025-05-11 17:27:41 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -36,3 +36,30 @@ gsap.utils.toArray("section").forEach(section => {
         }}
     )
 })
+const themeChanger = document.querySelector('.theme-change')
+themeChanger.addEventListener('click', () => {
+    let isLight = localStorage.getItem('theme') == 'light'
+    if (isLight){
+        localStorage.setItem('theme', 'dark')
+        themeChanger.innerHTML = '<i class="fas fa-moon"></i>'
+        document.documentElement.style.setProperty('--primary', '#837457')
+        document.documentElement.style.setProperty('--secondary', '#b39f77')
+        document.documentElement.style.setProperty('--background', '#151413')
+        document.documentElement.style.setProperty('--text', '#b0ad96')
+        document.querySelector("#EFT").src = "imgs/EFT.png"
+        document.querySelector("#getrdy").src = "imgs/getrdyfescape.png"
+        document.querySelector("#usecicon").src = "imgs/useclogo.png"
+        document.querySelector("#bearicon").src = "imgs/bearlogo.png"
+    }else{
+        localStorage.setItem('theme', 'light')
+        themeChanger.innerHTML = '<i class="fas fa-sun"></i>'
+        document.documentElement.style.setProperty('--primary', '#5e6980')
+        document.documentElement.style.setProperty('--secondary', '#4f5169')
+        document.documentElement.style.setProperty('--background', '#eaebec')
+        document.documentElement.style.setProperty('--text', '#4f5169')
+        document.querySelector("#EFT").src = "imgs/EFTinv.png"
+        document.querySelector("#getrdy").src = "imgs/getrdyfescapeinv.png"
+        document.querySelector("#usecicon").src = "imgs/usecblacklogo.png"
+        document.querySelector("#bearicon").src = "imgs/bearblacklogo.png"
+    }
+})
